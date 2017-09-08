d549984452c30747f63fba816dee5ad7f532d06d
supermacho <face_buk@mail.ru>
2017-09-08 14:08:27 +0300

    First commit

---
+++ TapCar/Helper.swift
@@ -0,0 +1,38 @@
+//
+//  Helper.swift
+//  TapCar
+//
+//  Created by supermacho on 01.09.17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColliderType {
+    static let CAR_COLLIDER: UInt32 = 0
+    static let ITEM_COLLIDER:UInt32 = 1
+    static let ITEM_COLLIDER_1:UInt32 = 2
+}
+
+
+class Helper: NSObject {
+    
+    func randomBetweenTwoNumbers(firstNumber: CGFloat, secondNumber: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNumber - secondNumber) + min(firstNumber, secondNumber)
+    }
+    
+    
+    
+    
+}
+
+class Settings {
+    static let sharedInstance = Settings()
+    
+    private init() {
+        
+        
+    }
+    var highScore = 0
+}
